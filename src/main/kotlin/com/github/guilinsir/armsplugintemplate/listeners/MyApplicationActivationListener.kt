3ad9fd3f97d02a09c9b2d3b06ab1e62b95e2b52e
GitHub Action <action@github.com>
2024-07-10 05:58:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/guilinsir/armsplugintemplate/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/guilinsir/armsplugintemplate/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.guilinsir.armsplugintemplate.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/guilinsir/armsplugintemplate/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/guilinsir/armsplugintemplate/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.guilinsir.armsplugintemplate.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.guilinsir.armsplugintemplate.MyBundle
+import com.github.guilinsir.armsplugintemplate.services.MyProjectService
 import javax.swing.JButton
 
 
